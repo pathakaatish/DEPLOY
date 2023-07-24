b98bc0cf7a8a6707dc22fd118d6b4501850cbd50
pathak_aatish_05 <125594836+pathakaatish@users.noreply.github.com>
2023-07-24 17:23:22 +0530

    Add files via upload

---
+++ Assignment1_py_basics.py
@@ -0,0 +1,129 @@
+# ASSIGNMENT NO.1
+
+#Q1 
+
+"""(*) # Is a multiplication mathematical Operator
+a=7*2
+print(a)
+
+('hello') # Is a String
+print('hello') #print(type('hello')) <class 'str'>
+
+(-87.9) # is a float
+a=-87.9
+print(type(a)) # <class 'float'>
+
+(-) # Is a subtract mathematical Operator
+a=7-2
+print(a)
+
+(/)  # Is a division mathematical Operator
+a=8/2
+print(a)
+
+(+) # Is a addition mathematical Operator
+a=8+2
+print(a)
+
+(6) # Is a Integer
+a=6
+print(type(a)) # <class 'int'>"""
+
+#Q2
+
+"""
+X='Aatish'
+Y=2
+print( X, type(X) )
+print( Y, type(Y) ) 
+print( ''' Difference between Variable & String is, String is collection of alphabets 
+or group of words and it cannot be changed after giving input. 
+Can define string using ('')or("") Ex=X='Aatish'. And Variable is used for the referce, to 
+store the value (int,float & complex num). Ex=Y=2 ''')
+
+#Q3
+
+#(Three types of Variables)
+
+A=6 
+print(A, type(A))  #integer 
+
+B=2.2
+print(B, type(B)) #float
+
+C=5+5j
+print(C, type(C)) #complex num 
+
+#Q4
+
+A=2*4+8
+print(A)
+print(''' Expression consists of values, variable and operator 
+and that needs to be evalute & solve to get a single value.''')
+
+#Q5
+
+An statement is a line of code for ex: print(), if-else 
+And expression is where we are performing operations like multiplication, addition and more
+
+#Q6
+
+bacon=22
+bacon+1
+print(bacon) 
+# Output will be 22  <class 'int'>
+# Variable bacon contain integer
+
+#Q7
+
+s='spam'+'spamspam'
+S='spam'*3
+print(s) # output : spamspamspam
+print(S) # output : spamspamspam
+
+#Q8
+
+print('''A variable should always start with alphbet or underscrore and not with the num that
+is why (eggs) is a valid variable and (100) is invalid''')
+
+#Q9
+
+int() # to get integer 
+a=int(input("Enter the num: ")) it will take input as int type
+float() # to get floting-point num
+b=float(input("Enter the num: ")) it will take input as float type
+str() # to get string
+c=str() it will take input as string
+
+#Q10
+
+expression='I have eatem'+99+'burritos.'
+
+'''The problem with the above expression is that we have two string values 
+and one integer value. (str) value can only concatenate to (str) not to (int)'''
+'''Expression consist of of values, variable and operator 
+and that needs to be evalute & solve to get a single value.'''
+"The way can fix it by coverting 'str' into 'int' "
+
+a='I have eaten'
+a=int()
+b=99
+c='burritos.'
+c=int()
+X=a+b+c
+print(X) # output is 99 
+
+OR ELSE 
+
+"The way can fix it by coverting 'int' into 'str' "
+
+a='I have eaten '
+#a=int()
+b=99
+c=' burritos.'
+#c=int()
+X=a+str(b)+c
+print(X) # output is I have eaten 99 burritos.
+"""
+
+
