f1950166826e0a239fd88cc5add6815c5f60a2a3
pathak_aatish_05 <125594836+pathakaatish@users.noreply.github.com>
2023-07-24 21:31:34 +0530

    Add files via upload

---
+++ assig_prog_26.py
@@ -0,0 +1,65 @@
+import random
+
+"""#Q1
+a="hello python"
+print(a)
+
+#Q2
+#(/)  # Is a division mathematical Operator
+div=8/2
+print(div)
+
+#(+) # Is a addition mathematical Operator
+add=8+2
+print(add)
+
+#Q3
+# Three sides of the triangle is a, b and c:  
+E = float(input('Enter first side: '))  
+F = float(input('Enter second side: '))  
+G = float(input('Enter third side: '))  
+  
+s = (E + F + G) / 2  
+  
+  
+area = (s*(s-E)*(s-F)*(s-G)) ** 0.5  
+print("The area of the triangle is %0.2f" %area)
+
+#Q4
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+z=x
+x=y
+y=z
+print(f"{x}, {y}")
+
+#Q5
+
+Z=random.randint(0,99) #importing random module 
+print(Z)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
